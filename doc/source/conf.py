965868082a2d6989c7a744b26dbc50831b8dd746
Luigi Toscano <ltoscano@redhat.com>
2019-01-14 23:40:24 +0100

    Post-import fixes: name, license, doc, translations
    - fix the name of the module in setup.cfg, according the correct
      pattern (otherwise the name of the directory is not correct);
    - add LICENSE file, required by (at least some) distributions;
    - setup the translations;
    - fix the name of the manpage, use the name of the module;
    - add the classifier for Python 3.6.
    
    Change-Id: Ie05394a8fa545dd9d7e0964087e5ff36dc266352

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# sahara-storm-plugin documentation build configuration file.
+# sahara-plugin-storm documentation build configuration file.
 #
 # -- General configuration -----------------------------------------------------
 
@@ -180,7 +180,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'sahara-storm-plugin.tex', u'sahara-storm-plugin Documentation',
+  ('index', 'sahara-plugin-storm.tex', u'sahara-plugin-storm Documentation',
    u'Sahara team', 'manual'),
 ]
 
@@ -210,7 +210,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'sahara-storm-plugin', u'sahara-storm-plugin Documentation',
+    ('index', 'sahara-plugin-storm', u'sahara-plugin-storm Documentation',
      [u'Sahara team'], 1)
 ]
 
@@ -224,8 +224,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'sahara-storm-plugin', u'sahara-storm-plugin Documentation',
-   u'Sahara team', 'sahara-storm-plugin', 'One line description of project.',
+  ('index', 'sahara-plugin-storm', u'sahara-plugin-storm Documentation',
+   u'Sahara team', 'sahara-plugin-storm', 'One line description of project.',
    'Miscellaneous'),
 ]
 
